280cbfea0e4f94b68aad39c8454f2fa49e47ff3e
IslamWahid <eng.islamwahid@gmail.com>
2018-12-11 19:28:40 +0200

    update redis file

--- src/redis.js
+++ src/redis.js
@@ -1,5 +1,17 @@
-var redis = process.env.NODE_ENV === 'test'
-  ? require('fakeredis')
-  : require('redis')
+var redis =
+  process.env.NODE_ENV === 'test' ? require('fakeredis') : require('redis')
 
-module.exports = redis.createClient
+var host = process.env.REDIS_HOST || '127.0.0.1'
+var port = process.env.REDIS_PORT || '6379'
+
+var client = redis.createClient(port, host)
+
+client.on('connect', function () {
+  console.log('Redis client connected')
+})
+
+client.on('error', function (err) {
+  console.log('Redis Error ' + err)
+})
+
+module.exports = client
