01475620d641039e0a4ff014cf70abed2267b161
IslamWahid <eng.islamwahid@gmail.com>
2018-12-11 15:23:58 +0200

    update controller

--- src/controllers/buyer.js
+++ src/controllers/buyer.js
@@ -1,8 +1,18 @@
+var body = require('body/json')
 var send = require('send-data/json')
 
 var buyerController = {
   post: (req, res, opts, cb) => {
-    send('done')
+    body(req, res, function (err, data) {
+      if (err) return cb(err)
+      console.log(data)
+      // send(req, res, {
+      //   body: {
+      //     foo: 'bar'
+      //   },
+      //   statusCode: 201
+      // })
+    })
   }
 }
 
