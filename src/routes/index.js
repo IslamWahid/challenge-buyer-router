7fb187b29b805ca4296862ff27d61f6c316ec19f
IslamWahid <eng.islamwahid@gmail.com>
2018-12-11 19:29:34 +0200

    add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,7 @@ var buyerController = require('../controllers/buyer')
 var router = HttpHashRouter()
 
 router.set('/buyers', { POST: buyerController.post })
+router.set('/buyers/:id', { GET: buyerController.get })
+router.set('/routes', { GET: buyerController.get })
 
 module.exports = router
